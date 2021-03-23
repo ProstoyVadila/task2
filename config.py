53613c2ce9c294c969fbca79ec96fb1dd01765b6
Vadim Gorbachev <vadim.gorbachev.main@yandex.ru>
2021-03-23 18:16:10 +0300

    cleaned config

--- config.py
+++ config.py
@@ -1,25 +1,11 @@
 BASE_URL = 'https://ru.wikipedia.org'
-TEST_URL = 'https://ru.wikipedia.org/w/index.php?title=Категория:Животные_по_алфавиту&from={letter}'
 URL = 'https://ru.wikipedia.org/w/index.php?title=Категория:Животные_по_алфавиту&from=А'
-
-LAST_URL = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%' \
-           'D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%' \
-           'D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&subcatfrom=%D0%AF&filefrom=%D0%A' \
-           'F&pagefrom=%D0%AF#mw-pages'
 
 LETTERS = (
   'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
   'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
   'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я'
 )
-
-
-NEXT_PAGE = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%' \
-            '82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D' \
-            '0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%8' \
-            '4%D0%B0%D0%B2%D0%B8%D1%82%D1%83&pagefrom=%D0%90%D0%B7%D0%BE' \
-            '%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BF%D1%83%D0%B3%D0%BE%D0%' \
-            'BB%D0%BE%D0%B2%D0%BA%D0%B0#mw-pages'
 
 HEADERS = [
     'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
